ebdc89251b7965a423b2a26fdb9f7d20393c056e
renzon <renzo@gmail.com>
2013-11-28 16:07:23 -0200

    Consertando arquivos para deploy

--- tg/wsgi.py
+++ tg/wsgi.py
@@ -7,13 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tg.settings")
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tg.settings")
 application = Cling(get_wsgi_application())
-
-
-
-
