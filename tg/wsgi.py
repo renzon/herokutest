b78228e3fa6a14d3109f2259b1f1ddccc59dcff9
renzon <renzo@gmail.com>
2013-11-28 15:50:16 -0200

    Setup inicial

---
+++ tg/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for tg project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tg.settings")
+application = Cling(get_wsgi_application())
+
+
+
+
